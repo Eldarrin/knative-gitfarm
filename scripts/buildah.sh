010622772c6b834c5de7b549dd04bc21af913c95
Eldarrin <32762846+Eldarrin@users.noreply.github.com>
2022-06-03 17:41:53 +0100

    globs

--- scripts/buildah.sh
+++ scripts/buildah.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #buildah unshare
 microcontainer=$(buildah from registry.access.redhat.com/ubi9-micro:9.0.0-11)
-micromount=$(buildah mount $microcontainer)
+micromount=$(buildah mount "$microcontainer")
 
 dnf install \
-    --installroot $micromount \
+    --installroot "$micromount" \
     --releasever 9 \
     --setopt install_weak_deps=false \
     --nodocs -y \
@@ -24,9 +24,9 @@ dnf install \
     krb5-libs \
     zlib \
     libicu --nogpgcheck
-dnf clean all --installroot $micromount
+dnf clean all --installroot "$micromount"
 
-mkdir $micromount/runner
+mkdir "$micromount/runner"
 
 mkdir tmp
 cd tmp
@@ -36,14 +36,14 @@ if [ ! -f "config.sh" ]; then
   rm -f runner.tar.gz
 fi
 
-cp -r * $micromount/runner/
-buildah config --workingdir /runner $microcontainer
+cp -r * "$micromount/runner/"
+buildah config --workingdir /runner "$microcontainer"
 cd ..
 
-buildah run $microcontainer adduser runner
-buildah run $microcontainer mkdir /opt/hostedtoolcache
-buildah run $microcontainer chmod g+rwx /opt/hostedtoolcache
-buildah config --port 8080 $microcontainer
+buildah run "$microcontainer" adduser runner
+buildah run "$microcontainer" mkdir /opt/hostedtoolcache
+buildah run "$microcontainer" chmod g+rwx /opt/hostedtoolcache
+buildah config --port 8080 "$microcontainer"
 
 cd ../cmd
 go get github.com/go-playground/webhooks/v6
@@ -52,16 +52,16 @@ go get golang.org/x/oauth2
 
 CGO_ENABLED=0 go build -o ../scripts/tmp/runnercontrol .
 cd ../scripts
-cp tmp/runnercontrol $micromount/runner/runnercontrol
-chmod +x $micromount/runner/runnercontrol
+cp tmp/runnercontrol "$micromount/runner/runnercontrol"
+chmod +x "$micromount/runner/runnercontrol"
 
-buildah run $microcontainer chown -R runner:runner /runner
-buildah config --user runner $microcontainer
+buildah run "$microcontainer" chown -R runner:runner /runner
+buildah config --user runner "$microcontainer"
 
-buildah config --entrypoint /runner/runnercontrol $microcontainer
+buildah config --entrypoint /runner/runnercontrol "$microcontainer"
 
-buildah umount $microcontainer
+buildah umount "$microcontainer"
 
-buildah commit $microcontainer git-main
+buildah commit "$microcontainer" git-main
 
-buildah rm $microcontainer
+buildah rm "$microcontainer"
